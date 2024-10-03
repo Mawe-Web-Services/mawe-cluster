eb55dac7b00dcb438f398e0ac0af6c63a9174282
Cyro MBLabs <cyroMBlabs@NMB-DESENV-421.local>
2024-10-03 00:25:47 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Iniciando um setInterval que executa a cada 5 segundos
+    /*setInterval(() => {
+      console.log('Servidor rodando...');
+    }, 5000);
+    */
+  
   await app.listen(3000);
 }
 bootstrap();
